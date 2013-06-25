4362ab32a5b8c9a7506788b071b1b1d709e10f75
Irineu <jrlicks@gmail.com>
2013-06-25 18:26:35 -0300

    Adicionando views de SignUp/SignIn/Reset

---
+++ app/controllers/User.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class User extends Controller {
+
+    public static void signup() {
+        render();
+    }
+
+    public static void signin() {
+        render();
+    }
+    
+    public static void reset() {
+        render();
+    }
+    
+}
